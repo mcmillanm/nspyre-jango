98f5bbcec4e33cbb997ff2f1077e70597b12da51
mcmillanm <marquism@uchicago.edu>
2023-09-19 13:51:47 -0500

    mcmillanm

--- src/template/drivers/local_inserv.py
+++ src/template/drivers/local_inserv.py
@@ -25,6 +25,7 @@ nspyre_init_logger(
 
 with InstrumentServer() as local_inserv, InstrumentGateway(port=42067) as remote_gw:
     local_inserv.add('subs', _HERE / 'subsystems_driver.py', 'SubsystemsDriver', args=[local_inserv, remote_gw], local_args=True)
+    local_inserv.add('daq', 'nspyre_drivers.ni.daq.py')
     local_inserv.add('odmr_driver', _HERE / 'fake_odmr_driver.py', 'FakeODMRInstrument')
     # run a CLI (command-line interface) that allows the user to enter
     # commands to control the server
